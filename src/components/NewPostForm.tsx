0534ebcfb4c615673ed0aff017f6750f1c3b18a0
ekfman <ekfirstman@gmail.com>
2023-09-27 22:24:45 -0700

    profile image

--- src/components/NewPostForm.tsx
+++ src/components/NewPostForm.tsx
@@ -1,12 +1,21 @@
+import { useSession } from "next-auth/react";
 import { Button } from "./Button";
+import { ProfileImage } from "./ProfileImage";
 
-export function NewPostForm () {
-    return <form className="flex flex-col gap-2 border-b px-4 py-2">
-        <div className="flex gap-4">
-            {/* <ProfileImage src="#url"/> */}
-            <textarea className="flex-grow resize-none overflow-hidden p-4 text-lg" placeholder="What's happening?"/>
-
-        </div>
-        <Button className="self-end">Post</Button>
+export function NewPostForm() {
+  const session = useSession();
+  if (session.status !== "authenticated") return; //this is saying to not render a new tweet, if we are not a logged in user
+  console.log(session.data.user.image);
+  return (
+    <form className="flex flex-col gap-2 border-b px-4 py-2">
+      <div className="flex gap-4">
+        <ProfileImage src={session.data.user.image} />
+        <textarea
+          className="flex-grow resize-none overflow-hidden p-4 text-lg"
+          placeholder="What's happening?"
+        />
+      </div>
+      <Button className="self-end">Post</Button>
     </form>
+  );
 }
