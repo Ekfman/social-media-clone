720b1770161a4fc03dcba7e93d45bfff22d76f74
ekfman <ekfirstman@gmail.com>
2023-11-12 11:29:57 -0800

    recent vs following tweet logic

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,6 +1,9 @@
+import { Prisma } from "@prisma/client";
+import { inferAsyncReturnType } from "@trpc/server";
 import { z } from "zod";
 
 import {
+  createTRPCContext,
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
@@ -10,53 +13,22 @@ export const postRouter = createTRPCRouter({
   infiniteFeed: publicProcedure
     .input(
       z.object({
+        onlyFollowing: z.boolean().optional(),
         limit: z.number().optional(),
         cursor: z.object({ id: z.string(), createdAt: z.date() }).optional(),
       }),
     )
-    .query(async ({ input: { limit = 10, cursor }, ctx }) => {
-      const currentUserId = ctx.session?.user.id;
-
-      const data = await ctx.db.post.findMany({
-        take: limit + 1,
-        cursor: cursor ? { createdAt_id: cursor } : undefined,
-        orderBy: [{ createdAt: "desc" }, { id: "desc" }],
-        select: {
-          id: true,
-          content: true,
-          createdAt: true,
-          _count: { select: { likes: true } },
-          likes:
-            currentUserId == null
-              ? false
-              : { where: { userId: currentUserId } },
-          user: {
-            select: { name: true, id: true, image: true },
-          },
-        },
-      });
-
-      let nextCursor: typeof cursor | undefined;
-      if (data.length > limit) {
-        const nextItem = data.pop();
-        if (nextItem != null) {
-          nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt };
-        }
-      }
-      return {
-        posts: data.map((post) => {
-          return {
-            id: post.id,
-            content: post.content,
-            createdAt: post.createdAt,
-            likeCount: post._count.likes,
-            user: post.user,
-            likedByMe: post.likes?.length > 0,
-          };
-        }),
-        nextCursor,
-      };
-    }),
+    .query(
+      async ({ input: { limit = 10, onlyFollowing = false, cursor }, ctx }) => {
+        const currentUserId = ctx.session?.user.id;
+        return await getInfinitePosts({
+          limit,
+          ctx,
+          cursor,
+          whereClause: currentUserId == null || !onlyFollowing ? undefined : {},
+        });
+      },
+    ),
   create: protectedProcedure
     .input(z.object({ text: z.string() }))
     .mutation(async ({ input: { text }, ctx }) => {
@@ -68,16 +40,68 @@ export const postRouter = createTRPCRouter({
   toggleLike: protectedProcedure
     .input(z.object({ id: z.string() }))
     .mutation(async ({ input: { id }, ctx }) => {
-      const data = {postId: id, userId: ctx.session.user.id}
+      const data = { postId: id, userId: ctx.session.user.id };
       const existingLike = await ctx.db.like.findUnique({
         where: { userId_postId: data },
       });
-      if(existingLike == null){
-        await ctx.db.like.create({data})
-        return {addedLike: true}
+      if (existingLike == null) {
+        await ctx.db.like.create({ data });
+        return { addedLike: true };
       } else {
-        await ctx.db.like.delete({where: {userId_postId: data}})
-        return {addedLike: false}
+        await ctx.db.like.delete({ where: { userId_postId: data } });
+        return { addedLike: false };
       }
     }),
 });
+
+async function getInfinitePosts({
+  whereClause,
+  ctx,
+  limit,
+  cursor,
+}: {
+  whereClause?: Prisma.PostWhereInput;
+  limit: number;
+  cursor: { id: string; createdAt: Date } | undefined;
+  ctx: inferAsyncReturnType<typeof createTRPCContext>;
+}) {
+  const currentUserId = ctx.session?.user.id;
+
+  const data = await ctx.db.post.findMany({
+    take: limit + 1,
+    cursor: cursor ? { createdAt_id: cursor } : undefined,
+    orderBy: [{ createdAt: "desc" }, { id: "desc" }],
+    select: {
+      id: true,
+      content: true,
+      createdAt: true,
+      _count: { select: { likes: true } },
+      likes:
+        currentUserId == null ? false : { where: { userId: currentUserId } },
+      user: {
+        select: { name: true, id: true, image: true },
+      },
+    },
+  });
+
+  let nextCursor: typeof cursor | undefined;
+  if (data.length > limit) {
+    const nextItem = data.pop();
+    if (nextItem != null) {
+      nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt };
+    }
+  }
+  return {
+    posts: data.map((post) => {
+      return {
+        id: post.id,
+        content: post.content,
+        createdAt: post.createdAt,
+        likeCount: post._count.likes,
+        user: post.user,
+        likedByMe: post.likes?.length > 0,
+      };
+    }),
+    nextCursor,
+  };
+}
